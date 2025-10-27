630107390d468e5b3e85db0998330fc6fd10a329
Pablo Hendrickx <pablo.hendrickx@atlascopco.com>
2025-10-27 20:06:11 +0100

    init commit

---
+++ src/lib/classes/blackhole.ts
@@ -0,0 +1,12 @@
+class BlackHole {
+    mass: number;
+    position: [Number, number];
+    radius: number
+
+    constructor(mass: number, position: [number,number]){
+        this.mass = mass
+        this.position = position
+        this.radius = 
+    }
+
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let canvas;
+  let gl;
+
+  onMount(() => {
+    gl = canvas.getContext('webgl');
+    if (!gl) {
+      alert('WebGL not supported');
+      return;
+    }
+
+    gl.clearColor(0.1, 0.1, 0.1, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  });
+</script>
+
+<canvas bind:this={canvas} width="800" height="600" style="display:block;margin:auto;"></canvas>
+
+<style>
+  canvas { background: #111; }
+</style>
