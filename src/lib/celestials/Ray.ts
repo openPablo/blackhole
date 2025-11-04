3e098a10bfd3d4db2426e8576c908ce34c4b3681
Pablo Hendrickx <pablo.hendrickx@atlascopco.com>
2025-11-04 22:21:01 +0100

    finish implementing 3d rays

--- src/lib/celestials/Ray.ts
+++ src/lib/celestials/Ray.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-const { sin, cos, acos, atan2 } = Math;
+const { sqrt, sin, cos, acos, atan2 } = Math;
 
 export class Ray {
 	pos: THREE.Vector3
@@ -20,24 +20,27 @@ export class Ray {
 	delta: number = 0.02; //Step size
 	eventHorizon: number;
 	maxTrail: number = 200;
+	E: number;
+
 	constructor(pos: THREE.Vector3, dir: THREE.Vector3, blackholeEventHorizon: number) {
 		this.pos = pos;
 		this.dir = dir;
 		this.eventHorizon = blackholeEventHorizon;
 
-		//Distance quotation
+		//Get spherical (polar) coords
 		this.r = this.pos.length()
 		this.phi = atan2(this.pos.y, this.pos.x);
 		this.theta = acos(this.pos.z/ this.r);
 
-		//Velocity polar positions m/s
+		// Get polar velocities
 		this.dr     = sin(this.theta)*cos(this.phi)*this.dir.x + sin(this.theta)*sin(this.phi)*this.dir.y + cos(this.theta)*this.dir.z;
 		this.dtheta = (cos(this.theta)*cos(this.phi)*this.dir.x + cos(this.theta)*sin(this.phi)*this.dir.y - sin(this.theta)*this.dir.z) / this.r;
 		this.dphi   = (-sin(this.phi)*this.dir.x + cos(this.phi)*this.dir.y) / (this.r * sin(this.theta));
 
-		this.dr = this.dir.x * cos(this.phi) + this.dir.y * sin(this.phi);
-		this.dphi  = ( -this.dir.x * sin(this.phi) + this.dir.y * cos(this.phi) ) / this.r;
-
+		// calc energy constant
+		let f = 1.0 - blackholeEventHorizon / this.r;
+		let dt_dlambda = sqrt((this.dr*this.dr)/f + this.r*this.r*this.dtheta*this.dtheta + this.r*this.r*sin(this.theta)*sin(this.theta)*this.dphi*this.dphi);
+		this.E = f * dt_dlambda;
 
 		this.trail.push(this.pos.clone());
 	}
@@ -46,11 +49,13 @@ export class Ray {
 		if (this.r  > this.eventHorizon){
 			
 			//Update velocity, close to blackhole -> faster and bend more
-			this.dr 	+= this.calcD2r() * this.delta;
-			this.dphi += this.calcD2phi() * this.delta;
+			this.dr 		+= this.calcD2r() * this.delta;
+			this.dphi 	+= this.calcD2phi() * this.delta;
+			this.dtheta += this.calcD2Theta() * this.delta;
 
-			this.r 		+= this.dr * this.delta;
-			this.phi 	+= this.dphi * this.delta;
+			this.r 			+= this.dr * this.delta;
+			this.phi 		+= this.dphi * this.delta;
+			this.theta += this.dtheta * this.delta;
 
 			//Polar positions to Cartesian
 			this.pos.x = this.r * sin(this.theta) * cos(this.phi);
@@ -71,20 +76,27 @@ export class Ray {
 		return this.points;
 	}
 
-	// Calculate second derivative of r (radial acceleration)
+	// Calculate second derivatives of r (radial acceleration)
+	// Geodesic equation for radial coordinate in Schwarzschild metric (2D approximation)
 	private calcD2r(): number {
-		// Geodesic equation for radial coordinate in Schwarzschild metric (2D approximation)
-		// d²r/dλ² = r(dφ/dλ)² - rs/(2r²) * (1 - rs/r) * c²
-		return this.r * this.dphi * this.dphi - (this.eventHorizon) / (2.0 * this.r * this.r);
+		let f = 1.0 - this.eventHorizon / this.r;
+		let dt_dlambda = this.E / f;
+		return 	- (this.eventHorizon / (2 * this.r * this.r)) * f * dt_dlambda * dt_dlambda
+						+ (this.eventHorizon / (2 * this.r * this.r * f)) * this.dr * this.dr
+						+ this.r * (this.dtheta * this.dtheta + sin(this.theta) * sin(this.theta) * this.dphi * this.dphi);
 
+	}
+	private calcD2Theta(): number {
+		return	- (2.0 / this.r) * this.dr * this.dtheta
+						+ sin(this.theta) * cos(this.theta) * this.dphi * this.dphi;
 	}
 
 	// Calculate second derivative of phi (angular acceleration)
 	private calcD2phi(): number {
-		// Geodesic equation for angular coordinate
-		// d²φ/dλ² = -2(dr/dλ)(dφ/dλ)/r
-		return -2.0 * this.dr * this.dphi / this.r;
+		return 	- (2.0 / this.r) * this.dr * this.dphi
+						- 2.0 * cos(this.theta) / sin(this.theta) * this.dtheta * this.dphi;
 	}
+
 
 	private updateGeometry() {
 		if (this.points.geometry) {
