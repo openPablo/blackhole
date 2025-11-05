029ff989726ff5848f6cd622506c08460de0a83c
pablo.hendrickx <pablo.hendrickx@atlascopco.com>
2025-11-05 20:36:56 +0100

    add adaptive step

--- README.md
+++ README.md
@@ -12,21 +12,7 @@ npm run dev
 I was inspired by this super interesting youtube video, in which the OP created a blackhole simulation using C++ and openGL.
 https://www.youtube.com/watch?v=8-B6ryuBkCM (great channel!)
 
-I saw an interesting comment giving math advice:
-``` 
-There is the lesser well-known brother of the geodesic equation.
 
-You can use the Hamiltonian H = 1/2 g^munu p_mu p_nu to get:
-1. d/dlambda q^mu = g^munu p_nu
-2. d/dlambda p_\alpha = -1/2 (dg^munu / dq^alpha) p_mu p_nu
-
-This way, not only are the geodesic equations much easier to derive, but conserved quatities, such as energy and angular momentum, are conserved during integration.
-
-Sidenote: p_mu p^mu = 0 is true analytically for light rays, but in praxis this is only true up to machine accuracy, i.e. p_mu p^mu = epsilon.
-You can NOT use this equation to simpllify the eoms unfortunately. 
-
-Also, you can consider using an integrator with variable stepsize. This will shorten simulation time significantly. 
-```
 So I decided to give it a go and try to implement a blackhole efficient enough, that it could be ran in the browser.
 
 I learned Threejs/webGL through this exercise :).
--- src/lib/celestials/Ray.ts
+++ src/lib/celestials/Ray.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 const { sqrt, sin, cos, acos, atan2 } = Math;
 
 export class Ray {
-	pos: THREE.Vector3
+	pos: THREE.Vector3;
 	dir: THREE.Vector3;
 	points: THREE.Points = new THREE.Points();
 	private trail: THREE.Vector3[] = [];
@@ -17,9 +17,9 @@ export class Ray {
 	dphi: number;
 	dtheta: number;
 
-	delta: number = 0.02; //Step size
+	stepSize = 0.05;
 	eventHorizon: number;
-	maxLength: number = 1000;
+	maxLength: number = 600;
 	E: number;
 
 	constructor(pos: THREE.Vector3, dir: THREE.Vector3, blackholeEventHorizon: number) {
@@ -28,48 +28,66 @@ export class Ray {
 		this.eventHorizon = blackholeEventHorizon;
 
 		//Get spherical (polar) coords
-		this.r = this.pos.length()
+		this.r = this.pos.length();
 		this.phi = atan2(this.pos.y, this.pos.x);
-		this.theta = acos(this.pos.z/ this.r);
+		this.theta = acos(this.pos.z / this.r);
 
 		// Get polar velocities
-		this.dr     = sin(this.theta)*cos(this.phi)*this.dir.x + sin(this.theta)*sin(this.phi)*this.dir.y + cos(this.theta)*this.dir.z;
-		this.dtheta = (cos(this.theta)*cos(this.phi)*this.dir.x + cos(this.theta)*sin(this.phi)*this.dir.y - sin(this.theta)*this.dir.z) / this.r;
-		this.dphi   = (-sin(this.phi)*this.dir.x + cos(this.phi)*this.dir.y) / (this.r * sin(this.theta));
+		this.dr =
+			sin(this.theta) * cos(this.phi) * this.dir.x +
+			sin(this.theta) * sin(this.phi) * this.dir.y +
+			cos(this.theta) * this.dir.z;
+		this.dtheta =
+			(cos(this.theta) * cos(this.phi) * this.dir.x +
+				cos(this.theta) * sin(this.phi) * this.dir.y -
+				sin(this.theta) * this.dir.z) /
+			this.r;
+		this.dphi =
+			(-sin(this.phi) * this.dir.x + cos(this.phi) * this.dir.y) / (this.r * sin(this.theta));
 
 		// calc energy constant
 		let f = 1.0 - blackholeEventHorizon / this.r;
-		let dt_dlambda = sqrt((this.dr*this.dr)/f + this.r*this.r*this.dtheta*this.dtheta + this.r*this.r*sin(this.theta)*sin(this.theta)*this.dphi*this.dphi);
+		let dt_dlambda = sqrt(
+			(this.dr * this.dr) / f +
+				this.r * this.r * this.dtheta * this.dtheta +
+				this.r * this.r * sin(this.theta) * sin(this.theta) * this.dphi * this.dphi
+		);
 		this.E = f * dt_dlambda;
 
 		this.trail.push(this.pos.clone());
 	}
 
 	step() {
-		if (this.trail.length >= this.maxLength) return;
-		if (this.r  > this.eventHorizon){
-			
-			//Update velocity, close to blackhole -> faster and bend more
-			this.dr 		+= this.calcD2r() * this.delta;
-			this.dphi 	+= this.calcD2phi() * this.delta;
-			this.dtheta += this.calcD2Theta() * this.delta;
+		if (this.trail.length >= this.maxLength || this.r < this.eventHorizon) return;
+		let adaptiveStep =
+			this.stepSize *
+			(1 / (1 + 2 * (this.eventHorizon / Math.max(this.r - this.eventHorizon, 1e-6))));
 
-			this.r 			+= this.dr * this.delta;
-			this.phi 		+= this.dphi * this.delta;
-			this.theta += this.dtheta * this.delta;
+		//Update velocity, close to blackhole -> faster and bend more
+		this.dr += this.calcD2r() * adaptiveStep;
+		this.dphi += this.calcD2phi() * adaptiveStep;
+		this.dtheta += this.calcD2Theta() * adaptiveStep;
 
-			//Polar positions to Cartesian
-			this.pos.x = this.r * sin(this.theta) * cos(this.phi);
-			this.pos.y = this.r * sin(this.theta) * sin(this.phi);
-			this.pos.z = this.r * cos(this.theta);
+		this.r += this.dr * adaptiveStep;
+		this.phi += this.dphi * adaptiveStep;
+		this.theta += this.dtheta * adaptiveStep;
 
-			this.trail.push(this.pos.clone());
-		}
+		//Polar positions to Cartesian
+		this.pos.x = this.r * sin(this.theta) * cos(this.phi);
+		this.pos.y = this.r * sin(this.theta) * sin(this.phi);
+		this.pos.z = this.r * cos(this.theta);
+
+		this.trail.push(this.pos.clone());
 		this.updateGeometry();
 	}
 	draw(): THREE.Points {
 		this.updateGeometry();
-		this.points.material = new THREE.PointsMaterial({color: 0xffffff, size: 0.02, transparent: true, opacity: 0.5});
+		this.points.material = new THREE.PointsMaterial({
+			color: 0xffffff,
+			size: 0.02,
+			transparent: true,
+			opacity: 0.5
+		});
 		return this.points;
 	}
 
@@ -78,22 +96,27 @@ export class Ray {
 	private calcD2r(): number {
 		let f = 1.0 - this.eventHorizon / this.r;
 		let dt_dlambda = this.E / f;
-		return 	- (this.eventHorizon / (2 * this.r * this.r)) * f * dt_dlambda * dt_dlambda
-						+ (this.eventHorizon / (2 * this.r * this.r * f)) * this.dr * this.dr
-						+ this.r * (this.dtheta * this.dtheta + sin(this.theta) * sin(this.theta) * this.dphi * this.dphi);
-
+		return (
+			-(this.eventHorizon / (2 * this.r * this.r)) * f * dt_dlambda * dt_dlambda +
+			(this.eventHorizon / (2 * this.r * this.r * f)) * this.dr * this.dr +
+			this.r *
+				(this.dtheta * this.dtheta + sin(this.theta) * sin(this.theta) * this.dphi * this.dphi)
+		);
 	}
 	private calcD2Theta(): number {
-		return	- (2.0 / this.r) * this.dr * this.dtheta
-						+ sin(this.theta) * cos(this.theta) * this.dphi * this.dphi;
+		return (
+			-(2.0 / this.r) * this.dr * this.dtheta +
+			sin(this.theta) * cos(this.theta) * this.dphi * this.dphi
+		);
 	}
 
 	// Calculate second derivative of phi (angular acceleration)
 	private calcD2phi(): number {
-		return 	- (2.0 / this.r) * this.dr * this.dphi
-						- 2.0 * cos(this.theta) / sin(this.theta) * this.dtheta * this.dphi;
+		return (
+			-(2.0 / this.r) * this.dr * this.dphi -
+			((2.0 * cos(this.theta)) / sin(this.theta)) * this.dtheta * this.dphi
+		);
 	}
-
 
 	private updateGeometry() {
 		if (this.points.geometry) {
