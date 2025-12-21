09a1e0a6e0780fe03c4621793169a48bdee97a00
pablo.hendrickx <pablo.hendrickx@atlascopco.com>
2025-12-21 14:28:02 +0100

    clean up code

--- src/lib/webgl/fragment.shader.ts
+++ src/lib/webgl/fragment.shader.ts
@@ -4,7 +4,6 @@ varying vec2 vUv;
 
 const float PI = 3.141592653589793238462643;
 const float sunRadius = 0.2;
-const float accretionThickness = 0.004;
 
 uniform float u_eventHorizon;
 uniform vec2 u_resolution;
@@ -14,13 +13,12 @@ uniform vec3 u_starPos;
 uniform sampler2D u_spaceTexture;
 uniform sampler2D u_starTexture;
 
-// d2R equation (Orbital Plane form)
-// derived from geodesic eq with theta=pi/2, dtheta=0
 float calcD2r(float E, float f, float r, float dr, float dphi){    
     return -(u_eventHorizon / (2.0 * r * r)) * (E * E / f)
     + (u_eventHorizon / (2.0 * r * r * f)) * dr * dr
     + r * f * dphi * dphi;
 }
+
 vec4 sampleSeamless(sampler2D tex, vec2 uv) {
   vec2 dX = dFdx(uv);
   vec2 dY = dFdy(uv);
@@ -28,86 +26,65 @@ vec4 sampleSeamless(sampler2D tex, vec2 uv) {
   if (abs(dY.x) > 0.5) dY.x = fract(dY.x + 0.5) - 0.5;
   return textureGrad(tex, uv, dX, dY);
 }
-void main() {
 
+void main() {
   vec2 uv = (vUv - 0.5) * 2.0;
   uv.x *= u_resolution.x / u_resolution.y;
 
   vec3 ray = u_camPos; 
   vec3 rayDir = normalize(mat3(u_viewMatrix) * vec3(uv, -1.0));
  
-  float r = length(ray);
-  float phi = 0.0;
- // We define a local coordinate system (X, Y) in the plane of the ray's motion.
-  vec3 orbitalX = normalize(ray); // Unit vector pointing to start position
+  vec2 polar = vec2(0.0, length(ray)); // x: phi, y: r
+  vec3 orbitalX = normalize(ray);
   vec3 angularMomentumVec = cross(ray, rayDir);
-  float h = length(angularMomentumVec); // Angular momentum per unit mass (conserved)
+  float h = length(angularMomentumVec);
   
   vec3 orbitalY;
-  
   if (h < 1e-6) {
-    // Arbitrary Y if radial (phi will stay 0 anyway)
     orbitalY = vec3(0.0, 1.0, 0.0); 
-    // If X is essentially Y, pick Z.
     if (abs(dot(orbitalX, orbitalY)) > 0.99) orbitalY = vec3(0.0, 0.0, 1.0);
   } else {
-    // Y is perpendicular to X and the Angular Momentum vector (Normal to plane)
-    vec3 orbitalZ = normalize(angularMomentumVec); // Normal to plane
-    orbitalY = cross(orbitalZ, orbitalX);
+    orbitalY = cross(normalize(angularMomentumVec), orbitalX);
   }
 
-  // Initial State in Orbital Frame
-  float dr = dot(rayDir, orbitalX);
-  float dphi = h / (r * r);
+  vec2 dPolar = vec2(h / (polar.y * polar.y), dot(rayDir, orbitalX)); // x: dphi, y: dr
 
-  // Energy Calculation (Conserved)
-  float f = 1.0 - u_eventHorizon / r;
-  float E = sqrt(dr * dr + f * h * h / (r * r));
+  float f = 1.0 - u_eventHorizon / polar.y;
+  float E = sqrt(dPolar.y * dPolar.y + f * h * h / (polar.y * polar.y));
 
-  vec2 finalUv = vec2 (0.0);
+  vec2 finalUv = vec2(0.0);
   int hitType = 0;
-
-  int i = 0;
   float step = 0.005;
-  while (i < 600 && r <= 1.1) {
-    ray = r * (cos(phi) * orbitalX + sin(phi) * orbitalY); // cartesian coords
 
-    if (r <= u_eventHorizon * 1.001) {
+  for (int i = 0; i < 600; i++) {
+    ray = polar.y * (cos(polar.x) * orbitalX + sin(polar.x) * orbitalY); // back to cartesian
+
+    if (polar.y <= u_eventHorizon * 1.001) {
       hitType = 1;
       break;
     }
 
-    if (r >= 1.0) {
-      finalUv = vec2(
-          atan(ray.z, ray.x) / (2.0 * PI) + 0.5, // u
-          asin(ray.y) / PI + 0.5                 // v
-        );
+    if (polar.y >= 1.0) {
+      finalUv = vec2(atan(ray.z, ray.x) / (2.0 * PI) + 0.5, asin(ray.y) / PI + 0.5);
       hitType = 2;
       break;
     }
-    if(r >= 0.35 && r<= 0.85 && distance(u_starPos, ray) <= sunRadius){
+
+    if (polar.y >= 0.35 && polar.y <= 0.85 && distance(u_starPos, ray) <= sunRadius) {
       vec3 relDir = normalize(ray - u_starPos);
-      finalUv = vec2(
-          atan(relDir.z, relDir.x) / (2.0 * PI) + 0.5, // u
-          asin(relDir.y) / PI + 0.5                    // v
-      );
+      finalUv = vec2(atan(relDir.z, relDir.x) / (2.0 * PI) + 0.5, asin(relDir.y) / PI + 0.5);
       hitType = 3;
       break;
     }
 
-    f = 1.0 - u_eventHorizon / r;
-  
-    dphi = h / (r * r);    
-    dr += calcD2r(E, f, r, dr, dphi) * step;
-    r += dr * step;
-    phi += dphi * step;
-    
-    i++;
+    f = 1.0 - u_eventHorizon / polar.y;
+    dPolar.x = h / (polar.y * polar.y);    
+    dPolar.y += calcD2r(E, f, polar.y, dPolar.y, dPolar.x) * step;
+    polar += dPolar * step;
   }
 
-  // setting the frag color outside the while loop solved a visual bug, by allowing the GPU to optimize
   if (hitType == 1) {
-    gl_FragColor = vec4(0.0,0.0,0.0,1.0);
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
   } else if (hitType == 2) {
     gl_FragColor = sampleSeamless(u_spaceTexture, finalUv);
   } else if (hitType == 3) {
