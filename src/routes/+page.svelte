c1b088335dc31b96676afbbcd795b53444f6f3e1
pablo.hendrickx <pablo.hendrickx@atlascopco.com>
2025-12-21 21:50:53 +0100

    use static adapter for static websites

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,8 +35,8 @@
 			u_camPos: { value: new THREE.Vector3() },
 			u_viewMatrix: { value: new THREE.Matrix4() },
 			u_starPos: { value: new Star().pos },
-			u_spaceTexture: { value: new THREE.TextureLoader().load('src/lib/assets/space.png') },
-			u_starTexture: { value: new THREE.TextureLoader().load('src/lib/assets/star1.png') }
+			u_spaceTexture: { value: new THREE.TextureLoader().load('space.png') },
+			u_starTexture: { value: new THREE.TextureLoader().load('star1.png') }
 		};
 		const quad = new THREE.Mesh(
 			new THREE.PlaneGeometry(2, 2),
